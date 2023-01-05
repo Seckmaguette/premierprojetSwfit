dc0737cb6a40f173040ecb251c70ebd2cecfb21d
maguette <seckmaguettte036@gmail.com>
2023-01-05 11:48:09 +0000

    Initial Commit

---
+++ PageConnexion/PageConnexionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageConnexionApp.swift
+//  PageConnexion
+//
+//  Created by Maguette SECK on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PageConnexionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
